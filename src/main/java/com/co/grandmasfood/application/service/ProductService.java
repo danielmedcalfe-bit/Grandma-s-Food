47b698deb5316d50bb675faeb2e5632a081b9a97
Daniel Stiven Medcalfe Grisales <daniel.medcalfe@globant.com>
2026-03-31 10:14:10 -0500

    verified

--- src/main/java/com/co/grandmasfood/application/service/ProductService.java
+++ src/main/java/com/co/grandmasfood/application/service/ProductService.java
@@ -31,7 +31,7 @@ public class ProductService implements CreateProductUseCase, GetProductUseCase, 
                         log.warn("Produc with code {} already exists",command.getCode());
                         return Mono.error( new ProductAlreadyExistsException(command.getCode()));
                     }
-                Product product=buildProduct(command);sspush
+                Product product=buildProduct(command);sspushsssdfgh
 
                  return productPersistencePort.save(product)
                          .doOnSuccess(savedProduct ->
